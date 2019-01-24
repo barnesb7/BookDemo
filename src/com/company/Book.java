9b8ba67472f2165725849e8efc81ca4acb109bed
Brian B <brianb@MacBook-Pro-4.local>
2019-01-24 14:10:07 -0500

    Adding Book class, constructor, and getter + setter methods

---
+++ src/com/company/Book.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Book {
+
+
+    private String title;
+    private String author;
+    private int numberOfPages;
+
+
+    public Book(String title, String author, int numberOfPages){
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+    }
+
+
+    public void printDetails(){
+        System.out.println( title + " by " + author + " has " + numberOfPages + " number of pages.");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public int getCopyrightDate() {
+        return copyrightDate;
+    }
+
+    public void setCopyrightDate(int copyrightDate) {
+        this.copyrightDate = copyrightDate;
+    }
+
+}
